14f8f84a70f19e96dee99910624d4984fc0b1657
kshoultz <jedi.kevo@gmail.com>
2022-05-16 10:01:24 -0500

    continue attempt to fail a test on jenkins.

--- lib/src/test/java/selenium/gradle/eclipse/mac/LibraryTest.java
+++ lib/src/test/java/selenium/gradle/eclipse/mac/LibraryTest.java
@@ -2,10 +2,29 @@ package selenium.gradle.eclipse.mac;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Tag;
 
 public class LibraryTest {
-    @Test
+
+    @Tag("fast")
+	@Test
     public void testAdd() {
     	assertEquals(43, Integer.sum(19, 23));
+    }
+	
+	@Tag("slow")
+    @Test
+    public void testAddMaxInteger() {
+        assertEquals(2147483646, Integer.sum(2147183646, 300000));
+    }
+ 
+    @Tag("fast")
+    @Test
+    public void testDivide() {
+        assertThrows(ArithmeticException.class, () -> {
+            Integer.divideUnsigned(42, 0);
+        });
     }
 }
